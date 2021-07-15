8b5335ac894921fb82c3f2f972c30127201a4d60
Alohadancemeow <nawongsak@gmail.com>
2021-07-15 16:39:57 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,70 @@
+import React, { useEffect } from "react";
+import Banner from "./components/Banner";
+import Cases from "./components/Cases";
+import Header from "./components/Header";
+import IntroOverlay from "./components/IntroOverlay";
+import "./styles/App.scss";
+
+import gsap from "gsap";
+
+function App() {
+
+
+  useEffect(() => {
+
+    // vh
+    let vh = window.innerHeight * .01
+    document.documentElement.style.setProperty('--vh', `${vh}px`)
+
+
+    // display body
+    gsap.to('body', 0, {
+      css: { visibility: 'visible' }
+    })
+
+    //Gsap timeline
+    const tl = gsap.timeline()
+    tl.from(".line span", 1.8, {
+      y: 100,
+      ease: 'power4.out',
+      delay: 1,
+      skewY: 7,
+      stagger: {
+        amount: 0.3
+      }
+    }).to(".overlay-top", 1.6, {
+      height: 0,
+      ease: 'expo.inOut',
+      stagger: 0.4
+    }).to(".overlay-bottom", 1.6, {
+      width: 0,
+      ease: 'expo.inOut',
+      delay: -0.8,
+      stagger: {
+        amount: 0.4
+      }
+    }).to(".intro-overlay", 0, {
+      css: { display: 'none' }
+    }).from(".case-image img", 1.6, {
+      scale: 1.4,
+      ease: 'expo.inOut',
+      delay: -2,
+      stagger: {
+        amount: 0.4
+      }
+    })
+
+
+  }, [])
+
+  return (
+    <div className='App'>
+      <IntroOverlay />
+      <Header />
+      <Banner />
+      <Cases />
+    </div>
+  );
+}
+
+export default App;
