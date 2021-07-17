fbfc289d80aecd75679b7c3adee03f72292f741c
Alohadancemeow <nawongsak@gmail.com>
2021-07-17 18:43:34 +0700

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,69 +1,94 @@
-import React, { useEffect } from "react";
-import Banner from "./components/Banner";
-import Cases from "./components/Cases";
-import Header from "./components/Header";
-import IntroOverlay from "./components/IntroOverlay";
+import React, { useEffect, useState } from "react";
+import { Route } from 'react-router-dom'
+import gsap from "gsap";
 import "./styles/App.scss";
 
-import gsap from "gsap";
+// components
+import Header from "./components/Header";
+import Navigation from "./components/Navigation";
+
+// pages
+import Home from "./pages/Home";
+import CaseStudy from './pages/CaseStudy'
+import Approach from './pages/Approach'
+import About from './pages/About'
+import Services from './pages/Services'
+
+// routes
+const routes = [
+  { path: '/', name: 'Home', Component: Home },
+  { path: '/case-study', name: 'Case Study', Component: CaseStudy },
+  { path: '/approach', name: 'Approach', Component: Approach },
+  { path: '/services', name: 'Services', Component: Services },
+  { path: '/about-us', name: 'About Us', Component: About },
+]
+
+function debounce(fn, ms) {
+  let timer;
+
+  return () => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      timer = null;
+      fn.apply(this, arguments);
+    }, ms);
+  };
+}
 
 function App() {
 
+  // # Display body
+  gsap.to('body', 0, {
+    css: { visibility: 'visible' }
+  })
+
+  // # State
+  const [demensions, setDemensions] = useState({
+    height: window.innerHeight,
+    width: window.innerWidth
+  })
 
   useEffect(() => {
 
     // vh
-    let vh = window.innerHeight * .01
+    let vh = demensions.height * .01
     document.documentElement.style.setProperty('--vh', `${vh}px`)
 
+    // resize
+    const debouncedHandleResize = debounce(function handleResize() {
+      setDemensions({
+        ...demensions,
+        height: window.innerHeight,
+        width: window.innerWidth
+      })
+    }, 1000)
 
-    // display body
-    gsap.to('body', 0, {
-      css: { visibility: 'visible' }
-    })
+    window.addEventListener('resize', debouncedHandleResize)
 
-    //Gsap timeline
-    const tl = gsap.timeline()
-    tl.from(".line span", 1.8, {
-      y: 100,
-      ease: 'power4.out',
-      delay: 1,
-      skewY: 7,
-      stagger: {
-        amount: 0.3
-      }
-    }).to(".overlay-top", 1.6, {
-      height: 0,
-      ease: 'expo.inOut',
-      stagger: 0.4
-    }).to(".overlay-bottom", 1.6, {
-      width: 0,
-      ease: 'expo.inOut',
-      delay: -0.8,
-      stagger: {
-        amount: 0.4
-      }
-    }).to(".intro-overlay", 0, {
-      css: { display: 'none' }
-    }).from(".case-image img", 1.6, {
-      scale: 1.4,
-      ease: 'expo.inOut',
-      delay: -2,
-      stagger: {
-        amount: 0.4
-      }
-    })
-
+    return () => {
+      window.removeEventListener('resize', debouncedHandleResize)
+    }
 
   }, [])
 
   return (
-    <div className='App'>
-      <IntroOverlay />
-      <Header />
-      <Banner />
-      <Cases />
-    </div>
+    <>
+      <Header demensions={demensions} />
+      <div className="App">
+        {
+          routes.map(({ path, Component }) => (
+            <Route
+              key={path}
+              exact
+              path={path}
+            >
+              <Component demensions={demensions} />
+            </Route>
+          ))
+        }
+      </div>
+      <Navigation />
+    </>
   );
 }
 
---
+++ src/pages/CaseStudy.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const CaseStudy = () => {
+    return (
+        <div className="page">
+            <div className="container">
+                <div className="row">
+                    <h2>This is caseStudy page</h2>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CaseStudy
